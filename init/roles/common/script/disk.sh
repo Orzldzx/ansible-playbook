3e6853239d9ced375193582a2c5db537fad07310
Orzldzx <orzldzx@gmail.com>
2016-08-17 06:15:41 +0000

    修改调整配置

--- init/roles/common/script/disk.sh
+++ init/roles/common/script/disk.sh
@@ -8,7 +8,7 @@ CHECK_EXIST=`/sbin/fdisk -l 2> /dev/null | grep -o "$DISK"`
 CHECK_DISK_EXIST=$(/sbin/fdisk -l 2> /dev/null | grep -o "$DISK[1-9]")
 [ ! "$CHECK_DISK_EXIST" ] || { echo "WARNING: ${CHECK_DISK_EXIST} is Partition already !"; exit 1; }
 
-/sbin/fdisk /dev/xvdb << EOF
+/sbin/fdisk $DISK << EOF
 d
 n
 p
